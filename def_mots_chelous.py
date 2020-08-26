74b2150a215a2c2023ece0a4a269a75ecfde5d2d
tom <thomas.cassagne20@gmail.com>
2020-08-26 23:33:09 +0200

    4

--- def_mots_chelous.py
+++ def_mots_chelous.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('C:/Users/utilisateur/Documents/3.PROJETS/P2/projet2_git/enron/sample_dataset.csv', sep = ";")
 
+print(data)
 
 def louche(mots_louches):
     chelou = data[mots_louches].value_counts()                        # Trouve le nombre de mail contenants le terme "trade"
-    print(chelou)                                                     # Print le résultat
+    print(chelou)
+    return chelou                                                    # Print le résultat
 
 def graph(mots_louches):
     chelou_graph = data[mots_louches].value_counts().plot.bar()       # Créée le graph lié a nombre de mail contenants le terme "trade"
@@ -17,4 +19,7 @@ def louche_graph(mots_louches):
     chelou = data[mots_louches].value_counts()                        
     print(chelou)
     chelou_graph = data[mots_louches].value_counts().plot.bar()       
-    plt.show(chelou_graph)                                            
+    plt.show(chelou_graph)          
+
+a = louche
+                               
